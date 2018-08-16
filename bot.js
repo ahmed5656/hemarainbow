c057f74d075831ebb3422e2321d251995b407210
ahmed5656 <42041981+ahmed5656@users.noreply.github.com>
2018-08-16 23:03:47 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const {Client} = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+	client.user.setGame(`HEMA Rainbow  `,'https://www.twitch.tv/v5bz')
+    setInterval(function(){
+        client.guilds.get('463281138538905600').roles.find('name', '✱♑🙦⦕ ⚡HEMA⚡⦖🙤♑✱').edit({color: 'RANDOM'})
+        client.guilds.get('463281138538905600').roles.find('name', '♚☭⦕𝕆ℕ𝕃𝕐-𝕀𝕀𝔸ℍ𝕄𝔼𝔻⦖☭♚').edit({color: 'RANDOM'})
+        client.guilds.get('463281138538905600').roles.find('name', '✨✭⦕•Staff Rainbow •⦖✭✨').edit({color: 'RANDOM'})
+		client.guilds.get('463281138538905600').roles.find('name', '╔⦅⧨⦆⦕ Hema Bot ⦖⦅⧨⦆╗').edit({color: 'RANDOM'})
+		client.guilds.get('465547785018802177').roles.find('name', 'Rainbow').edit({color: 'RANDOM'})
+		client.guilds.get('474222293992865793').roles.find('name', 'Rainbow').edit({color: 'RANDOM'})
+      client.guilds.get('474579280626581505').roles.find('name', 'Rainbow').edit({color: 'RANDOM'})
+      client.guilds.get('428228155309031424').roles.find('name', 'Rainbow').edit({color: 'RANDOM'})
+        
+    },5000);
+}).login('NDcwMDE3NzY5MDE5MDE1MTY5.Djx0WA.NGCuyc8GLgTD4oWl_ia_hM9VraI');
